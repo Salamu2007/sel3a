bd5b6d6cef4cc3494eff27366c697a7d49d17057
ISLAM Boukhendeg <boukhendegnour@gmail.com>
2024-07-22 19:30:01 +0100

    edit islam Now

---
+++ sil3ati/webApp/frontend/sel3a/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './layout/home/home.component';
+import { LoginComponent } from './layout/login/login.component';
+import { CreateAccountComponent } from './layout/create-account/create-account.component';
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'createAccount',component:CreateAccountComponent},
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ sil3ati/webApp/frontend/sel3a/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { CreateAccountComponent } from './layout/create-account/create-account.component';
+import { LoginComponent } from './layout/login/login.component';
+import { HomeComponent } from './layout/home/home.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateAccountComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
